5326b6fe74792f1d2fe0a53940e2f380688d19fb
黄振洪 <2976151305@qq.com>
2023-08-29 14:22:42 +0800

    feat: ✨api request

---
+++ client/api/types/schema.type.ts
@@ -0,0 +1,41 @@
+/**
+ * @example
+ * GET:/api/v1/chat/db/list
+ * =>
+ * GetChatDbListResponse
+ */
+
+export type DBType =
+  | 'mysql'
+  | 'duckdb'
+  | 'sqlite'
+  | 'mssql'
+  | 'clickhouse'
+  | 'oracle'
+  | 'postgresql'
+  | 'db2'
+  | 'access'
+  | 'mongodb'
+  | 'hbase'
+  | 'redis'
+  | 'cassandra'
+  | 'couchbase'
+  | (string & {});
+
+export type GetChatDbListResponse = {
+  comment: string;
+  db_host: string;
+  db_name: string;
+  db_path: string;
+  db_port: number;
+  db_pwd: string;
+  db_type: DBType;
+  db_user: string;
+}[];
+
+export type GetChatDbSupportTypeResponse = {
+  db_type: DBType;
+  is_file_db: boolean;
+}[];
+
+export type PostChatDbParams = Partial<GetChatDbListResponse[0] & { file_path: string }>;
